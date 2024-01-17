b2375330d260005aa21c488fcb35e8da24d9ef4d
Olie <ojimenez-gonzalez@unomaha.edu>
2024-01-17 01:59:44 -0600

    Rock Paper Scissors game

---
+++ RPS/Images/imgs.js
@@ -0,0 +1,6 @@
+const paperSign =
+  "https://symbl-world.akamaized.net/i/webp/3d/c0bfc28ef2db2eebf66879a2dc37ea.webp";
+const rockSign =
+  "https://symbl-world.akamaized.net/i/webp/3c/7890d4fc4e02ab5aeee4ba623fa563.webp";
+const scissorsSign =
+  "https://symbl-world.akamaized.net/i/webp/35/a3401a15f085c6ac84b0b9584a88eb.webp";
